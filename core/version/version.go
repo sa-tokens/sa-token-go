3835c6e2aa25539e223c545a810f9d4b818158c6
c <23@g>
2025-12-17 13:24:03 +0700

    更新群图片和版本

--- core/version/version.go
+++ core/version/version.go
@@ -3,5 +3,4 @@ package version
 // Version system level version number | 系统级版本号
 // This is the global version of Sa-Token-Go, modify this value to update the version across the entire project
 // 这是 Sa-Token-Go 的全局版本号，修改此值可更新整个项目的版本
-const Version = "0.1.5"
-
+const Version = "0.1.6"
